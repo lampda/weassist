882e5313d5e3f553246170ef1fc6308145da3353
Sheng Li <shengli@sussex.ac.uk>
2018-05-06 13:02:05 +0800

    released first version

---
+++ index.js
@@ -0,0 +1,123 @@
+const log4js = require('log4js')
+const fs = require('fs')
+const {
+    Wechaty
+} = require('wechaty')
+
+
+const listContacts(contact) {
+    const contactList = await bot.Contact.findAll()
+    console.log('Contact number: %d\n', contactList.length)
+}
+
+
+const makeFile = function(filename, data) {
+    fs.writeFile('./data/'+filename, data, {
+        'flag': 'a',
+        'encoding': 'utf8'
+    }, function(err) {
+        if (err) {
+            console.log('file not saved. ' + err)
+            return
+        } else {
+            console.log('file saved.')
+        }
+    })
+}
+
+// make user profile
+const createUserProfile = function(uid) {
+    //
+    console.log('normal user.')
+}
+
+
+const createMediaProfile = function(mid) {
+    //
+    console.log('media user.')
+}
+
+
+const saveChatMsg = function(dt_iso, message, filename) {
+    /* body... */
+    console.log(`${dt_iso} Message from ${message.from().name()}[${message.from().id}]: ${message}`)
+    makeFile(filename, `${dt_iso} ${message.from().name()}[${message.from().id}]: ${message}\n`)
+
+}
+
+
+const saveSelfMsg = function(dt_iso, message, filename) {
+    console.log(`${dt_iso} Message from ${message.from().name()}[${message.from().id}]: ${message}`)
+    makeFile(filename, `${dt_iso} ${message.from().name()}[${message.from().id}]: ${message}\n`)
+}
+
+
+const saveRoomMsg = function(dt_iso, message, filename) {
+    const room = message.room()
+    console.log(`${dt_iso} Message from ${message.from().name()}[${message.from().id}]: ${message}`)
+    makeFile(filename, `${dt_iso} ${message.from().name()}[${message.from().id}]: ${message}\n`)
+}
+
+
+const bot = Wechaty.instance()
+
+bot
+    .on('error', e => console.log('Bot error: %s', e))
+    .on('scan', (url, code) => console.log(`Scan QR Code to login: ${code}\n${url}`))
+    .on('login', user => console.log(`User ${user} logined`))
+    .on('friend', async function(contact, request) {
+        var dt = new Date()
+        var dt_iso = dt.toISOString()
+        if (request) {
+            console.log(`Friend requested from user ${contact.name()} [${contact.id()}] with ${request.hello}`)
+            const resp = await request.accept()
+            if (resp) {
+                console.log(`${dt_iso} Request from ${contact.name()} [${contact.id()}] is accept succesfully!`)
+                makeFile('main.log', `${dt_iso} ${contact.name()} [${contact.id()}]\n`)
+            } else {
+                console.log(`${dt_iso} Request from ${contact.name()} [${contact.id()}] failed to accept!`)
+            }
+        } else { // 2. confirm friend ship
+            console.log(`${dt_iso} new friendship confirmed with ${contact.name()} [${contact.id()}]`)
+        }
+        listContacts(contact)
+    })
+    .on('message', message => {
+        var dt = new Date()
+        var dt_iso = dt.toISOString()
+            // var userType = message.
+
+        var room = m.room()
+
+        if (room) {
+            console.log('room message received.')
+            saveRoomMsg(dt_iso, message, 'main_room_' + room + '.log')
+        } else if (message.self()) {
+            console.log('self message received.')
+            saveSelfMsg(dt_iso, message, 'main.log')
+        } else {
+            console.log('chat one2one message received.')
+            saveChatMsg(dt_iso, message, 'main_self.log')
+        }
+
+        console.log(`${dt_iso} ${message.from().name()}[${message.from().id}]: ${message}\n`)
+    })
+    .on('room-join', (room: Room, inviteeList: Contact[], inviter: Contact) => {
+        var dt = new Date()
+        var dt_iso = dt.toISOString()
+        const nameList = inviteeList.map(c => c.name()).join(',')
+        console.log(`Room ${room.topic()} got new member ${nameList}, invited by ${inviter}`)
+    })
+    .on('room-leave', (room: Room, leaverList: Contact[]) => {
+        var dt = new Date()
+        var dt_iso = dt.toISOString()
+        const nameList = leaverList.map(c => c.name()).join(',')
+        console.log(`Room ${room.topic()} lost member ${nameList}`)
+    })
+    .on('room-topic', (room: Room, topic: string, oldTopic: string, changer: Contact) => {
+        var dt = new Date()
+        var dt_iso = dt.toISOString()
+        console.log(`Room ${room.topic()} topic changed from ${oldTopic} to ${topic} by ${changer.name()}`)
+    })
+
+.start()
